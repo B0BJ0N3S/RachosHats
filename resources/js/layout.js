61cbc738d4f247def50a9649a7a12c2de1c8f749
Matthew <matthew6086888@hotmail.com>
2021-06-26 14:31:27 -0600

    Got header links to work

--- resources/js/layout.js
+++ resources/js/layout.js
@@ -7,11 +7,11 @@ class Header extends HTMLElement {
         this.innerHTML = `<header>
         <nav>
             <ul>
-                <li class="desktop"><a href="/RachosHats/index.html"><img src="/RachosHats/resources/images/001-frog.svg" alt=""></a></li>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#feature">Shop</a></li>
-                <li><a href="/RachosHats/html/about.html">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li class="desktop"><a href="./index.html"><img src="./resources/images/001-frog.svg" alt=""></a></li>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href=".">Shop</a></li>
+                <li><a href="./html/about.html">About</a></li>
+                <li><a href=".">Contact</a></li>
             </ul>
         </nav>
     </header>`;
@@ -46,11 +46,11 @@ class Contact extends HTMLElement {
         this.innerHTML = `<section class="contact">
         <h2>Contact</h2>
         <div class="container">
-            <img src="resources/images/email.png" alt="email icon"><span>businessEmail@gmail.com</span>
-            <img src="resources/images/telephone.png" alt="phone icon"><span>403-123-456</span>
-            <img src="resources/images/facebook.png" alt="facebook logo"><span><a href="fb.com">myFacebookAccount</a></span>
-            <img src="resources/images/instagram.png" alt="instagram logo"><span><a href="instagram.com">myInstagramAccount</a></span>
-            <img src="resources/images/snapchat.png" alt="snapchat logo"><span><a href="snapchat.com">mySnapchat</a></span>
+            <img src="./resources/images/email.png" alt="email icon"><span>businessEmail@gmail.com</span>
+            <img src="./resources/images/telephone.png" alt="phone icon"><span>403-123-456</span>
+            <img src="./resources/images/facebook.png" alt="facebook logo"><span><a href="fb.com">myFacebookAccount</a></span>
+            <img src="./resources/images/instagram.png" alt="instagram logo"><span><a href="instagram.com">myInstagramAccount</a></span>
+            <img src="./resources/images/snapchat.png" alt="snapchat logo"><span><a href="snapchat.com">mySnapchat</a></span>
         </div>
     </section>`;
     }
