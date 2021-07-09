27dad7106f603b0bb598afc91dd02fb3b178a03e
Matthew <matthew6086888@hotmail.com>
2021-07-09 14:56:49 -0600

    Added blinking cursor effect

--- resources/js/effects.js
+++ resources/js/effects.js
@@ -2,15 +2,40 @@
 // delay is in ms
 function typeWriter(docElement, delay = 70) {
     const txt = docElement.textContent;
-    docElement.textContent = '';
+    docElement.textContent = '|';
     let i = 0;
     write();
 
+    //writes text
     function write() {
-        docElement.textContent += txt.charAt(i);
+        //adding in letters and cursor
+        docElement.textContent = docElement.textContent.substring(0, i) + txt.charAt(i) + '|';
         i++;
+        //looping
         if (i < txt.length) {
             setTimeout(write, delay)
+        } else if (i == txt.length) {
+            blink();
+        }
+    }
+
+    //blinks the cursor 'nBlinks" times. Expects cursor is present when function called
+    function blink(nBlinks = 3) {
+        const blinkDelay = 500;
+        let blinkCount = 0;//counts number of blinks
+        removeCursor();
+
+        function addCursor() {
+            docElement.textContent += '|';
+            setTimeout(removeCursor, blinkDelay);
+        }
+
+        function removeCursor() {
+            docElement.textContent = docElement.textContent.substring(0, txt.length);
+            blinkCount++;
+            if (blinkCount <= nBlinks) {
+                setTimeout(addCursor, blinkDelay);
+            }
         }
     }
 }
